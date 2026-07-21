3a621f6476fddec6883edf93b3369d5b77b8adc4
Matvey <111543054+metrloopod2121@users.noreply.github.com>
2026-07-21 06:12:50 +0300

    Верхний отступ +5px

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -44,7 +44,7 @@ export default function RootLayout({
           <main
             className="flex-1 overflow-y-auto"
             style={{
-              paddingTop: "calc(var(--safe-top) + 24px)",
+              paddingTop: "calc(var(--safe-top) + 29px)",
               paddingBottom: "calc(var(--safe-bottom) + 82px)",
             }}
           >
